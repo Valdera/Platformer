de776a43125a2cf69f137ec8d09963b1c1c648bc
Fauzan Valdera <f.valdera@yahoo.co.id>
2021-06-10 16:11:55 +0700

    first commit

---
+++ src/scenes/Game.js
@@ -0,0 +1,32 @@
+import Phaser from 'phaser';
+import Hero from '../entities/Hero';
+
+class Game extends Phaser.Scene {
+  constructor() {
+    super({ key: 'GameScene' });
+  }
+
+  init(data) {}
+
+  preload() {
+    this.load.spritesheet('hero-run-sheet', 'assets/hero/run.png', {
+      frameWidth: 32,
+      frameHeight: 64
+    });
+  }
+
+  create(data) {
+    this.anims.create({
+      key: 'hero-running',
+      frames: this.anims.generateFrameNumbers('hero-run-sheet'),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    this.hero = new Hero(this, 250, 160);
+  }
+
+  update(time, delta) {}
+}
+
+export default Game;
